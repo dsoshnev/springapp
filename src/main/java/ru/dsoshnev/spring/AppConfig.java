d02c1b6cc8f4b86d1fdccedfff0f1cfa5321a759
dsoshnev <dsoshnev@mail.ru>
2021-02-20 21:46:05 +0300

    Initial commit

---
+++ src/main/java/ru/dsoshnev/spring/AppConfig.java
@@ -0,0 +1,9 @@
+package ru.dsoshnev.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"ru.dsoshnev.spring"})
+public class AppConfig {
+}
---
+++ src/main/java/ru/dsoshnev/spring/MainApp.java
@@ -0,0 +1,10 @@
+package ru.dsoshnev.spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    }
+}
